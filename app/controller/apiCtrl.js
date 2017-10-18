bb1397496e193f9e0f42cac376dcccbd6833e97d
boszkl8er <kittipong.iti17@gmail.com>
2017-10-18 14:10:55 +0700

    d

--- app/controller/apiCtrl.js
+++ app/controller/apiCtrl.js
@@ -1,11 +1,11 @@
 'use strict';
 angular.module('LfApp')
 .controller('apiCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {
-	/*$scope.getToken2 = function(){
+	$scope.getToken3 = function(){
 		 var resource = adalService.getClientID();
 		 console.log('ClientID = '+resource);
 		 var tokenVal = adalService.getCachedToken(resource);
 		 return tokenVal;
-	};*/
+	};
 
 }]);
