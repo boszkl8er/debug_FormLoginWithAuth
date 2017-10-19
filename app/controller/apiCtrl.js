a2a0ae97313f2a8366dec808750b98fe790cf425
boszkl8er <kittipong.iti17@gmail.com>
2017-10-19 11:55:57 +0700

    Debug

--- app/controller/apiCtrl.js
+++ app/controller/apiCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('LfApp')
 .controller('apiCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {
-	$scope.getToken3 = function(){
+	$scope.getTokenAPI = function(){
 		 var resource = adalService.getClientID();
 		 console.log('ClientID = '+resource);
 		 var tokenVal = adalService.getCachedToken(resource);
